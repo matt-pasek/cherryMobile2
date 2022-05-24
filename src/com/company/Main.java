312ddc30585168486a798740bbad8e7ddc1293ac
matt-pasek <mpasek@tm1.edu.pl>
2022-05-24 16:27:44 +0200

    magic commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,8 @@ public class Main {
         try {
             SQL.setup();
             //ClientUpdate.CreateBusinessClient("894-307-81-49", "Poland", "SproSoft");
-            //ClientUpdate.CreatePrivateClient("Anna", "Lis", "06243005875", "sagata@o2.pl");
+
+            ClientUpdate.CreatePrivateClient("Anna", "Lis", "06243005875", "sagata@o2.pl");
             //ClientUpdate.CreatePrivateClient("Anna", "Lis", "59121124832", "fagata@o2.pl");
             //ClientUpdate.DeleteBusinessClient("894-307-81-49");
             ClientUpdate.DeletePrivateClient("06243005875");
